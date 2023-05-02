247b883561ce68f47b2f59278c5239726a212dc0
Natchapon <NWorakunsap@kerrylogistics.com>
2023-05-02 09:48:44 +0700

    create react typescript app

---
+++ src/Routes.tsx
@@ -0,0 +1,13 @@
+import { Routes as ReactRoutes, Route } from "react-router-dom";
+
+export default function Routes() {
+  return (
+    <div className="app">
+      <ReactRoutes></ReactRoutes>
+    </div>
+  );
+}
+
+const layout = () => {
+  return <div></div>;
+};
